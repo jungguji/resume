8fc86ee934f27b6b6dad891a77aceea492dd6f32
junggu <junggu.ji.dev@gmail.com>
2020-05-24 12:20:16 +0900

    type : Feat p -> span 변경에 따라 배열 변경

--- payload/introduce.ts
+++ payload/introduce.ts
@@ -5,8 +5,8 @@ const introduce: IIntroduce.Payload = {
   disable: false,
 
   contents: [
-    'JAVA 개발자로 2년 8개월 살았습니다.',
-    'Spring 기반의 LG CNS Devon framework 프로젝트, Spring + Hibernate 프로젝트 1번, Enovia 프로젝트 4번 총 6개의 프로젝트 참여해봤습니다.',
+    'JAVA 개발자로 2년 8개월 살았습니다. ',
+    'Spring 기반의 LG CNS Devon framework 프로젝트, Spring + Hibernate 프로젝트 1번, Enovia 솔루션 프로젝트 4번 총 6개의 프로젝트 참여해봤습니다.',
     '이슈 트래커에서 일감 혹은 이슈를 정리 하는게 너무 좋고 재밌습니다.',
     '귀찮거나 반복되는 건 기계가 해야 한다고 생각합니다.',
     '그래서 귀찮거나 반복되는 일을 대신 해주는 기능이나 도구를 만드는 걸 좋아합니다.',
