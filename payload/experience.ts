899666ea81f9a114aa6a65e3c2330f3804ad7634
JG.JI <junggu.ji.dev@gmail.com>
2023-01-17 23:20:20 +0900

    Feat : 유몸멘트 경력 추가

--- payload/experience.ts
+++ payload/experience.ts
@@ -8,12 +8,13 @@ const experience: IExperience.Payload = {
       position: 'Server Back-end Developer',
       startedAt: '2021-01',
       descriptions: [
-        '8년 째 서비스 중인 예비 신랑, 신부를 위한 결혼 준비 서비스인 웨딩의 여신 개발 및 유지보수',
-        'Spring + Dojo + Hibernate 기반의 자체 framework를 이용한 MES 시스템 개발',
-        'Java를 이용한 SOAP WebService를 활용한 이종간 Interface 기능 개발',
-        'OODB내에 Tree구조로 저장된 Data 파싱하여 RDB에 저장하는 기능 개발',
+        '8년 째 운영 중인 Java Spring + JPA(Hibernate) + MySQL 기반 서비스의 Back-end 개발 및 유지보수',
+        'AWS EC2(CLB), RDS, S3 환경의 시스템 운영',
+        '내부망 Repository(Nexus)을 이용한 공통코드 라이브러리화, Shell script를 이용한 배포, Exception Log Slack 연동 등 개발환경 개선',
+        '멀티 스레드를 이용한 푸시 시스템 개선 (8시간 -> 30분)',
+        '업체용 서비스, 백오피스 등 레거시 서비스 개선 및 신규 개발',
       ],
-      skillKeywords: ['Java', 'Spring', 'Spring Boot', 'MySQL', 'Hibernate', 'AWS', 'Ubuntu OS'],
+      skillKeywords: ['Java', 'Spring', 'Spring Boot', 'MySQL', 'Hibernate', 'AWS'],
     },
     {
       title: '티엔씨파트너',
