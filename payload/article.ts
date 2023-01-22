633b043488adcdcb1ef6cbe56c9c777b328b4aac
JG.JI <junggu.ji.dev@gmail.com>
2023-01-22 23:25:23 +0900

    Refactor : 아티클 수정

--- payload/article.ts
+++ payload/article.ts
@@ -4,21 +4,25 @@ const article: IArticle.Payload = {
   disable: false,
   list: [
     {
-      content: '(2020. 11. 16) Synchronous, Asynchronous와 Blocking, Non-Blocking',
+      content: '(2022. 12. 26) JPA CustomRepository 작성 시 유의사항',
       href:
-        'https://jungguji.github.io/2020/11/16/Synchronous-Asynchronous%EC%99%80-Blocking-Non-Blocking/',
+        'https://jungguji.github.io/2022/12/26/JPA-CustomRepository-%EC%9E%91%EC%84%B1-%EC%8B%9C-%EC%9C%A0%EC%9D%98%EC%82%AC%ED%95%AD/',
     },
     {
-      content: '(2020. 05. 30) Spring Controller Junit으로 Test하기 ',
-      href: 'https://jungguji.github.io/2020/05/30/Spring-Controller-Junit-Test/',
+      content: '(2022. 11. 30) H2 DB에서 2D 좌표 저장하기',
+      href:
+        'https://jungguji.github.io/2022/11/30/H2-DB%EC%97%90%EC%84%9C-2D-%EC%A2%8C%ED%91%9C-%EC%A0%80%EC%9E%A5%ED%95%98%EA%B8%B0/',
     },
     {
-      content: '(2020. 05. 05) Ajax http status 415 Unsupported Media Type',
-      href: 'https://jungguji.github.io/2020/05/05/ajax-content-type-error/',
+      content: '(2022. 10. 16) Intelli J 코드 자동생성(with Live Template)',
+      href:
+        'https://jungguji.github.io/2022/10/16/Intelli-J-%EC%BD%94%EB%93%9C-%EC%9E%90%EB%8F%99%EC%83%9D%EC%84%B1-with-Live-Template/',
     },
     {
-      content: '(2020. 04. 15) Spring boot + thymeleaf custom error page 적용',
-      href: 'https://jungguji.github.io/2020/04/15/custom-error-page/',
+      content:
+        '(2021. 04. 19) org.junit.platform.commons.JUnitException: TestEngine with ID junit-vintage failed to discover tests',
+      href:
+        'https://jungguji.github.io/2021/04/19/org-junit-platform-commons-JUnitException-TestEngine-with-ID-junit-vintage-failed-to-discover-tests/',
     },
   ],
 };
