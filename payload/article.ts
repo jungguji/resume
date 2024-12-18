42512c6c611c90ab82c945360de57f8240e52fbf
JG.JI <junggu.ji.dev@gmail.com>
2024-12-18 12:48:41 +0900

    feat: 아티클 수정 - 테스트 코드 작성을 더 쉽게

--- payload/article.ts
+++ payload/article.ts
@@ -4,6 +4,12 @@ const article: IArticle.Payload = {
   disable: false,
   list: [
     {
+      content:
+        '(2024. 11. 19) 테스트 코드 작성을 더 쉽게 (with. Fixture Monkey) ',
+      href:
+        'https://jungguji.github.io/2024/11/19/%ED%85%8C%EC%8A%A4%ED%8A%B8-%EC%BD%94%EB%93%9C-%EC%9E%91%EC%84%B1%EC%9D%84-%EB%8D%94-%EC%89%BD%EA%B2%8C-with-Fixture-Monkey/',
+    },
+    {
       content: '(2022. 12. 26) JPA CustomRepository 작성 시 유의사항',
       href:
         'https://jungguji.github.io/2022/12/26/JPA-CustomRepository-%EC%9E%91%EC%84%B1-%EC%8B%9C-%EC%9C%A0%EC%9D%98%EC%82%AC%ED%95%AD/',
@@ -17,13 +23,7 @@ const article: IArticle.Payload = {
       content: '(2022. 10. 16) Intelli J 코드 자동생성(with Live Template)',
       href:
         'https://jungguji.github.io/2022/10/16/Intelli-J-%EC%BD%94%EB%93%9C-%EC%9E%90%EB%8F%99%EC%83%9D%EC%84%B1-with-Live-Template/',
-    },
-    {
-      content:
-        '(2021. 04. 19) org.junit.platform.commons.JUnitException: TestEngine with ID junit-vintage failed to discover tests',
-      href:
-        'https://jungguji.github.io/2021/04/19/org-junit-platform-commons-JUnitException-TestEngine-with-ID-junit-vintage-failed-to-discover-tests/',
-    },
+    }
   ],
 };
 
